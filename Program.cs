a25c9b2fe4b95416b4d736d7f93f3b6150efcbe0
MaxKhramtsov <maxkhramtsov13@gmail.com>
2023-02-14 16:13:09 +0300

    homework004

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write($"Enter the number-> ");
+int weekNumber = Convert.ToInt32( Console.ReadLine());
+
+if ( weekNumber == 6 || weekNumber == 7)
+{
+	System.Console.Write($" Day off ");
+}
+else if ( weekNumber < 1 || weekNumber > 7)
+{
+	System.Console.Write($" There is no such day of the week ");
+}
+else if ( weekNumber == 1 || weekNumber < 6 )
+{
+System.Console.Write($" Working day ");
+}
